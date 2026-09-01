b301d4692253e352262d4b39d1022af292cbe105
sapnac2020-sys <sapnac2020@gmail.com>
2026-09-01 17:19:47 +0530

    Verify existing acupuncture master count

--- ops/run-acupuncture-correlation-ledger.php
+++ ops/run-acupuncture-correlation-ledger.php
@@ -5,7 +5,7 @@ $config=require $argv[1]; $sql=file_get_contents($argv[2]);
 $pdo=new PDO($config['dsn'],$config['username'],$config['password'],[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
 $pdo->exec($sql);
 $out=$pdo->query("SELECT
- (SELECT COUNT(*) FROM ilb_acupuncture_point_standard) acupuncture_points,
+ (SELECT COUNT(*) FROM ilb_acupuncture_point) acupuncture_points,
  (SELECT COUNT(*) FROM ilb_aatmn_acupuncture_crosswalk) crosswalks,
  (SELECT COUNT(*) FROM ilb_aatmn_formula_hypothesis) formula_hypotheses")->fetch(PDO::FETCH_ASSOC);
 echo json_encode($out, JSON_PRETTY_PRINT), PHP_EOL;
